5bb03ea740c9aa6891af2ff3be7f12b88e0736fe
Phoenix <117663444@qq.com>
2017-03-09 22:24:26 +0800

    使用 Views

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 import express from 'express'
 import morgan from 'morgan'
+import path from 'path'
 
 const app = express()
+
+app.set('views', path.resolve(__dirname, 'views'))
+app.set('view engine', 'ejs')
 
 app.use(morgan('dev'))
 
 app.get('/', (request, response) => {
-  response.send('hello')
+  response.render('index')
 })
 
 app.listen(3000, () => {
