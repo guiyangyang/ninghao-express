88973930b61fba6fd926faf3efa409e7e8323df7
Phoenix <117663444@qq.com>
2017-03-10 10:21:33 +0800

    发表评论

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import morgan from 'morgan'
 import path from 'path'
+import bodyParser from 'body-parser'
 
 const app = express()
 
@@ -8,6 +9,10 @@ app.set('views', path.resolve(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
 app.use(morgan('dev'))
+app.use(bodyParser.urlencoded({ extended: false }))
+
+let comments = []
+app.locals.comments = comments
 
 app.get('/', (request, response) => {
   response.render('index')
@@ -15,6 +20,18 @@ app.get('/', (request, response) => {
 
 app.get('/comments/new', (request, response) => {
   response.render('comments/new')
+})
+
+app.post('/comments/new', (request, response) => {
+  if (!request.body.comment) {
+    response.status(400).send('Do you have something to say ?')
+    return
+  }
+  comments.push({
+    comment: request.body.comment,
+    created_at: new Date()
+  })
+  response.redirect('/comments')
 })
 
 app.listen(3000, () => {
